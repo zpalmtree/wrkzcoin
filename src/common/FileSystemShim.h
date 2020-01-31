5f4dd72678442f6863a3f264d018221df8945495
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-02-01 06:56:24 +0700

    update FileSystemShim

--- src/common/FileSystemShim.h
+++ src/common/FileSystemShim.h
@@ -5,9 +5,17 @@
 #pragma once
 
 #if __has_include(<filesystem>)
-#include <filesystem>
 #if !defined(__ANDROID__)
+#include <filesystem>
 namespace fs = std::filesystem;
+#else
+#include <filesystem/include/ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
+#endif
+#elif __has_include(<experimental/filesystem>)
+#if !defined(__ANDROID__)
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
 #else
 #include <filesystem/include/ghc/filesystem.hpp>
 namespace fs = ghc::filesystem;
